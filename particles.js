358be82f462f6dbe9357bf76b37c8bac9d37ab70
Stefan Tolken <stefantolkeng6@gmail.com>
2023-10-25 16:21:53 +0200

    changed the particle logic

--- particles.js
+++ particles.js
@@ -1,108 +1,65 @@
-// particles.js
+var colorArray = ["var(--accent1T)", "var(--accent2T)"];
 
-const numOfPlanets = 10;
-const particleSize = 100;
-var colorArray = ["var(--accent1)", "var(--accent2)"];
-// var colorArray = [
-//   "aliceblue",
-//   "aqua",
-//   "aquamarine",
-//   "azure",
-//   "blueviolet",
-//   "blue",
-//   "cadetblue",
-//   "chartreuse",
-//   "coral",
-//   "cornflowerblue",
-//   "cyan",
-//   "crimson",
-//   "darkblue",
-//   "darkcyan",
-//   "darkgoldenrod",
-//   "darkmagenta",
-//   "darkseagreen",
-//   "darkorange",
-//   "darkslateblue",
-//   "darkorchid",
-//   "darkslategray",
-//   "darkturquoise",
-//   "deeppink",
-//   "darkviolet",
-//   "dodgerblue",
-//   "floralwhite",
-//   "fuchsia",
-//   "gold",
-//   "greenyellow",
-//   "honeydew",
-//   "hotpink",
-//   "indigo",
-//   "lawngreen",
-//   "lightblue",
-//   "lightcyan",
-//   "lightgreen",
-//   "lightskyblue",
-//   "lightsalmon",
-//   "lightseagreen",
-//   "mediumslateblue",
-//   "mediumpurple",
-//   "midnightblue",
-//   "paleturquoise",
-//   "palegreen",
-// ];
+const skillsetDiv = document.querySelector(".skillset");
 
-const skillsetBlob = document.getElementById("skillsetBlob");
+const particles = [];
 
-for (let i = 0; i < numOfPlanets; i++) {
-  const particle = document.createElement("span");
-  skillsetBlob.parentNode.insertBefore(particle, skillsetBlob);
+for (let i = 0; i < 10; i++) {
+  const particle = document.createElement("div");
+  particle.classList.add("particle");
 
-  const randDistanceFromSVG = Math.random() * 20 + 50;
-  const angle = Math.random() * 2 * Math.PI;
-  const xPos =
-    skillsetBlob.getBoundingClientRect().width / 2 +
-    randDistanceFromSVG * Math.cos(angle);
-  const yPos =
-    skillsetBlob.getBoundingClientRect().height / 2 +
-    randDistanceFromSVG * Math.sin(angle);
+  applyRandomStyles(particle);
+  skillsetDiv.appendChild(particle);
 
-  particle.style.position = "absolute";
-  particle.style.left = "50%";
-  particle.style.top = "50%";
-  particle.style.transform = `translate(-50%, -50%) rotate(${angle}rad)`;
-  particle.style.zIndex = "10";
+  // Associate movement attributes with the particle
+  particles.push(createParticleMovement(particle));
+}
 
-  particle.style.width = `${particleSize}px`;
-  particle.style.height = `${particleSize}px`;
+function applyRandomStyles(element) {
+  // Random position within skillsetDiv
+  const maxX = skillsetDiv.clientWidth - 230; // 230 is the width of particle
+  const maxY = skillsetDiv.clientHeight - 90; // 90 is the height of particle
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
 
-  const randomColorIndex = Math.floor(Math.random() * colorArray.length);
-  const randomColor = colorArray[randomColorIndex];
+  // Random color from the colorArray
+  const randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
 
-  // Create the child particle
-  const childParticle = document.createElement("div");
-  childParticle.classList.add("child-particle");
-  childParticle.style.position = "absolute";
-  childParticle.style.left = `${xPos}px`;
-  childParticle.style.top = `${yPos}px`;
-  childParticle.style.backgroundColor = randomColor;
+  // Apply the styles to the element
+  element.style.left = randomX + "px";
+  element.style.top = randomY + "px";
+  element.style.backgroundColor = randomColor;
+  element.style.position = "absolute";
+}
 
-  const randInterval = Math.random() * 30 + 10; // Adjust for desired rotation time
-  childParticle.style.animation = `spinOp ${randInterval}s linear infinite`;
+// Animate particles
+animateParticles(particles);
 
-  // Create image and text inside child particle
-  const imgElement = document.createElement("img");
-  imgElement.src = "images/profile.jpg";
-  imgElement.style.height = "100%";
-  childParticle.appendChild(imgElement);
+function createParticleMovement(particle) {
+  return {
+    element: particle,
+    deltaX: Math.random() > 0.5 ? 1 : -1, // 1 means move right, -1 means move left
+    deltaY: Math.random() > 0.5 ? 1 : -1, // 1 means move down, -1 means move up
+  };
+}
 
-  const textElement = document.createElement("p");
-  textElement.innerText = "NA";
-  textElement.style.textAlign = "center";
-  textElement.style.fontSize = "20px";
-  textElement.style.color = "var(--main)";
-  textElement.style.fontWeight = "bold";
-  childParticle.appendChild(textElement);
+function animateParticles(particles) {
+  particles.forEach((p) => {
+    let rect = p.element.getBoundingClientRect();
+    let parentRect = skillsetDiv.getBoundingClientRect();
 
-  particle.appendChild(childParticle);
+    // Bounce off the edges by reversing the delta
+    if (rect.left <= parentRect.left || rect.right >= parentRect.right) {
+      p.deltaX = -p.deltaX;
+    }
+    if (rect.top <= parentRect.top || rect.bottom >= parentRect.bottom) {
+      p.deltaY = -p.deltaY;
+    }
 
-  particle.style.animation = `spin ${randInterval}s linear infinite`;
+    // Move the particle
+    p.element.style.left = p.element.offsetLeft + p.deltaX + "px";
+    p.element.style.top = p.element.offsetTop + p.deltaY + "px";
+  });
+
+  requestAnimationFrame(() => animateParticles(particles));
 }
